4428f313020cb3127f30b9795a3c42e45103aadf
walterwhite81 <walter81@protonmail.com>
2018-09-26 16:49:24 +0200

    small layout changes

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 1 : 1;
+    return testnet ? 0 : 0;
 }
 
 std::string static inline ToString(const CService &ip) {
