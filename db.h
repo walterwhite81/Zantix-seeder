72bdbdf3ab539aa9f1781f3b6ac0788bd494bddb
walterwhite81 <walter81@protonmail.com>
2018-09-26 13:50:37 +0200

    blocks update

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 0;
+    return testnet ? 1 : 1;
 }
 
 std::string static inline ToString(const CService &ip) {
