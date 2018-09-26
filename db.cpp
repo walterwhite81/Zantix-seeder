2ec3b41a77877f172b1a56de1511f05405ecd3e7
walterwhite81 <walter81@protonmail.com>
2018-09-26 14:22:35 +0200

    enable debug

--- db.cpp
+++ db.cpp
@@ -23,11 +23,11 @@ void CAddrInfo::Update(bool good) {
   stat1M.Update(good, age, 3600*24*30);
   int ign = GetIgnoreTime();
   if (ign && (ignoreTill==0 || ignoreTill < ign+now)) ignoreTill = ign+now;
-//  printf("%s: got %s result: success=%i/%i; 2H:%.2f%%-%.2f%%(%.2f) 8H:%.2f%%-%.2f%%(%.2f) 1D:%.2f%%-%.2f%%(%.2f) 1W:%.2f%%-%.2f%%(%.2f) \n", ToString(ip).c_str(), good ? "good" : "bad", success, total, 
-//  100.0 * stat2H.reliability, 100.0 * (stat2H.reliability + 1.0 - stat2H.weight), stat2H.count,
-//  100.0 * stat8H.reliability, 100.0 * (stat8H.reliability + 1.0 - stat8H.weight), stat8H.count,
-//  100.0 * stat1D.reliability, 100.0 * (stat1D.reliability + 1.0 - stat1D.weight), stat1D.count,
-//  100.0 * stat1W.reliability, 100.0 * (stat1W.reliability + 1.0 - stat1W.weight), stat1W.count);
+  printf("%s: got %s result: success=%i/%i; 2H:%.2f%%-%.2f%%(%.2f) 8H:%.2f%%-%.2f%%(%.2f) 1D:%.2f%%-%.2f%%(%.2f) 1W:%.2f%%-%.2f%%(%.2f) \n", ToString(ip).c_str(), good ? "good" : "bad", success, total,
+  100.0 * stat2H.reliability, 100.0 * (stat2H.reliability + 1.0 - stat2H.weight), stat2H.count,
+  100.0 * stat8H.reliability, 100.0 * (stat8H.reliability + 1.0 - stat8H.weight), stat8H.count,
+  100.0 * stat1D.reliability, 100.0 * (stat1D.reliability + 1.0 - stat1D.weight), stat1D.count,
+  100.0 * stat1W.reliability, 100.0 * (stat1W.reliability + 1.0 - stat1W.weight), stat1W.count);
 }
 
 bool CAddrDb::Get_(CServiceResult &ip, int &wait) {
@@ -81,7 +81,7 @@ void CAddrDb::Good_(const CService &addr, int clientV, std::string clientSV, int
   info.Update(true);
   if (info.IsGood() && goodId.count(id)==0) {
     goodId.insert(id);
-//    printf("%s: good; %i good nodes now\n", ToString(addr).c_str(), (int)goodId.size());
+    printf("%s: good; %i good nodes now\n", ToString(addr).c_str(), (int)goodId.size());
   }
   nDirty++;
   ourId.push_back(id);
@@ -97,11 +97,11 @@ void CAddrDb::Bad_(const CService &addr, int ban)
   uint32_t now = time(NULL);
   int ter = info.GetBanTime();
   if (ter) {
-//    printf("%s: terrible\n", ToString(addr).c_str());
+    printf("%s: terrible\n", ToString(addr).c_str());
     if (ban < ter) ban = ter;
   }
   if (ban > 0) {
-//    printf("%s: ban for %i seconds\n", ToString(addr).c_str(), ban);
+    printf("%s: ban for %i seconds\n", ToString(addr).c_str(), ban);
     banned[info.ip] = ban + now;
     ipToId.erase(info.ip);
     goodId.erase(id);
@@ -109,7 +109,7 @@ void CAddrDb::Bad_(const CService &addr, int ban)
   } else {
     if (/*!info.IsGood() && */ goodId.count(id)==1) {
       goodId.erase(id);
-//      printf("%s: not good; %i good nodes left\n", ToString(addr).c_str(), (int)goodId.size());
+      printf("%s: not good; %i good nodes left\n", ToString(addr).c_str(), (int)goodId.size());
     }
     ourId.push_back(id);
   }
